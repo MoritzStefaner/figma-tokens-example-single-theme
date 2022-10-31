9bbca20820b176648b1423a4e843d76d5939f079
MoritzStefaner <MoritzStefaner@users.noreply.github.com>
2022-10-31 16:56:48 +0000

    Update Tokens

---
+++ js/variables.js
@@ -0,0 +1,16 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 31 Oct 2022 16:56:47 GMT
+ */
+
+export const TokenSetOrder0 = "global";
+export const ColorsRed300 = "#FCA5A5";
+export const ColorsRed500 = "#EF4444";
+export const ColorsRed700 = "#B91C1C";
+export const ColorsBlack = "#000000";
+export const ColorsWhite = "#ffffff";
+export const SpacingSm = 4;
+export const SpacingMd = 8;
+export const SpacingLg = 16;
+export const SpacingXl = 32;
+export const Green500 = "#549b49";
