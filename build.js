077c83e5be766e4a2d766ad326a22be59ef75525
Moritz Stefaner <moritz@stefaner.eu>
2022-11-04 09:34:56 +0100

    remove custom build for now

--- build.js
+++ build.js
@@ -2,35 +2,35 @@ const StyleDictionaryPackage = require('style-dictionary');
 
 // custom formats etc
 
-StyleDictionaryPackage.registerFormat({
-  name: 'css/variables',
-  formatter: function (dictionary, config) {
-    return `${this.selector} {
-        ${dictionary.allProperties
-          .map((prop) => `  --${prop.name}: ${prop.value};`)
-          .join('\n')}
-      }`;
-  },
-});
+// StyleDictionaryPackage.registerFormat({
+//   name: 'css/variables',
+//   formatter: function (dictionary, config) {
+//     return `${this.selector} {
+//         ${dictionary.allProperties
+//           .map((prop) => `  --${prop.name}: ${prop.value};`)
+//           .join('\n')}
+//       }`;
+//   },
+// });
 
-StyleDictionaryPackage.registerTransform({
-  name: 'sizes/px',
-  type: 'value',
-  matcher: function (prop) {
-    // You can be more specific here if you only want 'em' units for font sizes
-    return [
-      'fontSize',
-      'spacing',
-      'borderRadius',
-      'borderWidth',
-      'sizing',
-    ].includes(prop.attributes.category);
-  },
-  transformer: function (prop) {
-    // You can also modify the value here if you want to convert pixels to ems
-    return parseFloat(prop.original.value) + 'px';
-  },
-});
+// StyleDictionaryPackage.registerTransform({
+//   name: 'sizes/px',
+//   type: 'value',
+//   matcher: function (prop) {
+//     // You can be more specific here if you only want 'em' units for font sizes
+//     return [
+//       'fontSize',
+//       'spacing',
+//       'borderRadius',
+//       'borderWidth',
+//       'sizing',
+//     ].includes(prop.attributes.category);
+//   },
+//   transformer: function (prop) {
+//     // You can also modify the value here if you want to convert pixels to ems
+//     return parseFloat(prop.original.value) + 'px';
+//   },
+// });
 
 // make config
 // function so it could also take parameters later (theme, …)
@@ -65,18 +65,18 @@ function getStyleDictionaryConfig() {
       },
 
       // custom combination of transformers
-      custom: {
-        transforms: ['attribute/cti', 'name/cti/kebab', 'sizes/px'],
-        buildPath: `02_output/css/`,
-        files: [
-          {
-            destination: `tokens-custom.css`,
-            format: 'css/variables',
-            // custom wrapper
-            selector: `.custom`,
-          },
-        ],
-      },
+      // custom: {
+      //   transforms: ['attribute/cti', 'name/cti/kebab', 'sizes/px'],
+      //   buildPath: `02_output/css/`,
+      //   files: [
+      //     {
+      //       destination: `tokens-custom.css`,
+      //       format: 'css/variables',
+      //       // custom wrapper
+      //       selector: `.custom`,
+      //     },
+      //   ],
+      // },
     },
   };
 }
