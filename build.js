3af80377685e430cc844114a42196dd1768a17a7
Moritz Stefaner <moritz@stefaner.eu>
2022-11-08 14:59:05 +0100

    improved build

--- build.js
+++ build.js
@@ -1,36 +1,29 @@
 const StyleDictionaryPackage = require('style-dictionary');
 
-// custom formats etc
-
-// StyleDictionaryPackage.registerFormat({
-//   name: 'css/variables',
-//   formatter: function (dictionary, config) {
-//     return `${this.selector} {
-//         ${dictionary.allProperties
-//           .map((prop) => `  --${prop.name}: ${prop.value};`)
-//           .join('\n')}
-//       }`;
-//   },
-// });
-
-// StyleDictionaryPackage.registerTransform({
-//   name: 'sizes/px',
-//   type: 'value',
-//   matcher: function (prop) {
-//     // You can be more specific here if you only want 'em' units for font sizes
-//     return [
-//       'fontSize',
-//       'spacing',
-//       'borderRadius',
-//       'borderWidth',
-//       'sizing',
-//     ].includes(prop.attributes.category);
-//   },
-//   transformer: function (prop) {
-//     // You can also modify the value here if you want to convert pixels to ems
-//     return parseFloat(prop.original.value) + 'px';
-//   },
-// });
+StyleDictionaryPackage.registerTransform({
+  name: 'sizes/px',
+  type: 'value',
+  matcher: function (prop) {
+    // You can be more specific here if you e.g. only want 'em' units for font sizes
+    return [
+      'fontSize',
+      'fontSizes',
+      'spacing',
+      'borderRadius',
+      'borderWidth',
+      'sizing',
+      'breakpoint',
+      'space',
+      'x',
+      'y',
+      'blur',
+      'spread',
+    ].includes(prop.original.type);
+  },
+  transformer: function (prop) {
+    return parseFloat(prop.original.value) + 'px';
+  },
+});
 
 // make config
 // function so it could also take parameters later (theme, …)
@@ -41,13 +34,23 @@ function getStyleDictionaryConfig() {
     platforms: {
       // default css build
       css: {
-        transformGroup: 'css',
+        transformGroup: 'web',
         buildPath: `02_output/css/`,
+        transforms: [
+          'attribute/cti',
+          'name/cti/kebab',
+          'sizes/px',
+          'color/css',
+          'time/seconds',
+        ],
         files: [
           {
             destination: 'tokens.css',
             format: 'css/variables',
-            selector: `:root`,
+            options: {
+              showFileHeader: false,
+              outputReferences: true,
+            },
           },
         ],
       },
@@ -56,27 +59,17 @@ function getStyleDictionaryConfig() {
       js: {
         transformGroup: 'js',
         buildPath: `02_output/js/`,
+        transforms: ['attribute/cti', 'name/cti/pascal', 'color/hex'],
         files: [
           {
             destination: 'tokens.js',
             format: 'javascript/es6',
+            options: {
+              showFileHeader: false,
+            },
           },
         ],
       },
-
-      // custom combination of transformers
-      // custom: {
-      //   transforms: ['attribute/cti', 'name/cti/kebab', 'sizes/px'],
-      //   buildPath: `02_output/css/`,
-      //   files: [
-      //     {
-      //       destination: `tokens-custom.css`,
-      //       format: 'css/variables',
-      //       // custom wrapper
-      //       selector: `.custom`,
-      //     },
-      //   ],
-      // },
     },
   };
 }
