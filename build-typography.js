ab0ba33fd843ed5679dce74e33c772d3a1bcd962
Moritz Stefaner <moritz@stefaner.eu>
2022-11-08 15:48:58 +0100

    add custom build for typography (css classes), effects

---
+++ build-typography.js
@@ -0,0 +1,78 @@
+const StyleDictionaryPackage = require('style-dictionary');
+
+const config = {
+  source: ['./01_intermediate/typography-styles.json'],
+  platforms: {
+    css: {
+      buildPath: './02_output/css/',
+      files: [
+        {
+          destination: 'typography-style-classes.css',
+          format: 'css/typographyClasses',
+          selector: ':root',
+          filter: (token) => token.type === 'typography',
+          options: {
+            showFileHeader: false,
+          },
+        },
+      ],
+    },
+  },
+};
+
+const kebabize = (str) =>
+  str.replace(
+    /[A-Z]+(?![a-z])|[A-Z]/g,
+    ($, ofs) => (ofs ? '-' : '') + $.toLowerCase()
+  );
+
+function convertToVariableIfNeeded(value) {
+  if (value.toString().startsWith('{') && value.toString().endsWith('}')) {
+    return `var(--${kebabize(value.slice(1, -1).split('.').join('-'))})`;
+  }
+  return value;
+}
+
+/**
+ * Format for css typography classes
+ * This generates theme-independent css classes so we're fine with just using css variables here
+ */
+
+StyleDictionaryPackage.registerFormat({
+  name: 'css/typographyClasses',
+  formatter: (dictionary, config) =>
+    dictionary.allProperties
+      .map(
+        (prop) => `/*
+  ${prop.name}
+  ${prop.original.comment}
+*/
+
+.typography-style-${kebabize(prop.name)} {
+  font-family: ${convertToVariableIfNeeded(prop.original.value.fontFamily)};
+  font-size: ${convertToVariableIfNeeded(prop.original.value.fontSize)};
+  font-weight: ${convertToVariableIfNeeded(prop.original.value.fontWeight)};
+  line-height: ${convertToVariableIfNeeded(prop.original.value.lineHeight)};
+  letter-spacing: ${convertToVariableIfNeeded(
+    prop.original.value.letterSpacing
+  )};
+  text-transform: ${convertToVariableIfNeeded(prop.original.value.textCase)};
+  text-decoration: ${convertToVariableIfNeeded(
+    prop.original.value.textDecoration
+  )};
+}
+`
+      )
+      .join('\n'),
+});
+
+// transform description attributes into comments,
+// see https://github.com/six7/figma-tokens/issues/499
+StyleDictionaryPackage.registerParser({
+  pattern: /\.json$/,
+  parse: ({ contents }) => {
+    return JSON.parse(contents.replaceAll('"description"', '"comment"'));
+  },
+});
+
+StyleDictionaryPackage.extend(config).buildAllPlatforms();
--- build.js
+++ build.js
@@ -1,36 +1,11 @@
 const StyleDictionaryPackage = require('style-dictionary');
 
-StyleDictionaryPackage.registerTransform({
-  name: 'sizes/px',
-  type: 'value',
-  matcher: function (prop) {
-    // You can be more specific here if you e.g. only want 'em' units for font sizes
-    return [
-      'fontSize',
-      'fontSizes',
-      'spacing',
-      'borderRadius',
-      'borderWidth',
-      'sizing',
-      'breakpoint',
-      'space',
-      'x',
-      'y',
-      'blur',
-      'spread',
-    ].includes(prop.original.type);
-  },
-  transformer: function (prop) {
-    return parseFloat(prop.original.value) + 'px';
-  },
-});
-
-// make config
-// function so it could also take parameters later (theme, …)
-
 function getStyleDictionaryConfig() {
   return {
-    source: ['01_intermediate/**/*.json'],
+    source: [
+      '01_intermediate/style-dict-tokens.json',
+      // '01_intermediate/typography-styles-expanded.json', // in case you want to add explicit versions of all typography style tokens
+    ],
     platforms: {
       // default css build
       css: {
@@ -42,6 +17,7 @@ function getStyleDictionaryConfig() {
           'sizes/px',
           'color/css',
           'time/seconds',
+          'shadow/shorthand',
         ],
         files: [
           {
@@ -73,6 +49,62 @@ function getStyleDictionaryConfig() {
     },
   };
 }
+
+/*
+
+  Custom transforms
+ 
+*/
+
+StyleDictionaryPackage.registerTransform({
+  name: 'sizes/px',
+  type: 'value',
+  matcher: function (prop) {
+    // You can be more specific here if you e.g. only want 'em' units for font sizes
+    return [
+      'fontSize',
+      'fontSizes',
+      'spacing',
+      'borderRadius',
+      'borderWidth',
+      'sizing',
+      'breakpoint',
+      'space',
+      'x',
+      'y',
+      'blur',
+      'spread',
+    ].includes(prop.original.type);
+  },
+  transformer: function (prop) {
+    return parseFloat(prop.original.value) + 'px';
+  },
+});
+
+/**
+ * Transform shadow shorthands for css variables
+ */
+
+/**
+ * This currently works fine if every value uses an alias, but if any one of these use a raw value, it will not be transformed.
+ */
+
+function transformShadow(shadow) {
+  const { x, y, blur, spread, color } = shadow;
+  return `${x}px ${y}px ${blur}px ${spread}px ${color}`;
+}
+
+StyleDictionaryPackage.registerTransform({
+  name: 'shadow/shorthand',
+  type: 'value',
+  transitive: true,
+  matcher: (token) => ['boxShadow'].includes(token.type),
+  transformer: (token) => {
+    return Array.isArray(token.original.value)
+      ? token.original.value.map((single) => transformShadow(single)).join(', ')
+      : transformShadow(token.original.value);
+  },
+});
 
 // transform description attributes into comments,
 // see https://github.com/six7/figma-tokens/issues/499
