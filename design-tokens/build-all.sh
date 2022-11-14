7d7a99deca90411de8c5eb8b2506fa970035064e
Moritz Stefaner <moritz@stefaner.eu>
2022-11-14 15:34:30 +0100

    multi-theme-demo

--- design-tokens/build-all.sh
+++ design-tokens/build-all.sh
@@ -3,9 +3,4 @@
 ./build-prepare-tokens.sh
 
 node ./scripts/build-base.js
-node ./scripts/build-docs-typography.js
-node ./scripts/build-docs.js
-node ./scripts/build-effects.js
-node ./scripts/build-fluid-sizing.js
-node ./scripts/build-full-dict.js
 node ./scripts/build-typography.js
--- design-tokens/build-prepare-tokens.sh
+++ design-tokens/build-prepare-tokens.sh
@@ -14,25 +14,6 @@ token-transformer \
 01_intermediate/typography-styles-dynamic.json \
 global,typographyStyles,dynamicSize,sizeLarge
 
-# # typography
-# token-transformer \
-# --resolveReferences=true --expandTypography=true \
-# 00_input/figma-tokens.json \
-# 01_intermediate/typography-styles-expanded.json \
-# global,typographyStyles,dynamicSize,sizeLarge
-
-# typography
-token-transformer \
-00_input/figma-tokens.json \
-01_intermediate/typography-styles-large.json \
-global,typographyStyles,dynamicSize,sizeLarge
-
-# typography
-token-transformer \
-00_input/figma-tokens.json \
-01_intermediate/typography-styles-small.json \
-global,typographyStyles,dynamicSize,sizeSmall
-
 # dark theme
 token-transformer \
 00_input/figma-tokens.json \
@@ -58,31 +39,4 @@ token-transformer \
 00_input/figma-tokens.json \
 01_intermediate/size-small.json \
 sizeSmall,dynamicSize
-
-# process _docs
-token-transformer \
---resolveReferences=false \
-00_input/figma-tokens.json \
-01_intermediate/_docs.json \
-global,_palettes,_docs,themeLight,sizeLarge,dynamicSize \
-
-# wrap large theme in theme name
-cat ./01_intermediate/size-large.json \
-| jq '{"size":{"large": .size}}' \
-> ./01_intermediate/namespaced-size-large.json
-
-# wrap small theme in theme name
-cat ./01_intermediate/size-small.json \
-| jq '{"size":{"small": .size}}' \
-> ./01_intermediate/namespaced-size-small.json
-
-# wrap dark theme in theme name
-cat ./01_intermediate/theme-dark.json \
-| jq '{"color":{"dark": .color}}' \
-> ./01_intermediate/namespaced-theme-dark.json
-
-# wrap light theme in theme name
-cat ./01_intermediate/theme-light.json \
-| jq '{"color":{"light": .color}}' \
-> ./01_intermediate/namespaced-theme-light.json
 
